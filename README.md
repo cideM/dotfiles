2c060ab321bf5bed7e83885d99a54f10311be1a2
Florian Beeres <yuuki@protonmail.com>
2020-07-21 13:00:31 +0200

    Updates: README, Fish and pkgs
    - Update pkgs
    - Update setup file and include Fish
    - Update README

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Dotfiles
 
 There's no place like `~`
+
+## Intro
+
+This repository holds the configuration for 3 hosts:
+
+-   Macbook Pro (MBP)
+-   NixOS installation on my desktop computer
+-   Arch installation on my desktop computer
+
+Both MBP and Arch have [Home Manager](https://github.com/rycee/home-manager) installed, but only to install `niv` and `lorri` and manage project dependencies with Nix. I do have a `home.nix` for my Arch installation, which works, but right now I don't see a point in mixing Pacman/Yay and Home Manager/Nix for my OS configuration and user packages.
+
+All Nix configuration lives in `/nix`.
+
+There is a lot of duplication right now. For example, I have a separate Neovim `init.vim` under `/nix` and under `/src`. Maybe at some point I'll decide to rely entirely on Nix for these things but for now I'm not willing to make that switch.
+
+## Setup
+
+For MBP and Arch, make sure to install [`minpac`](https://github.com/k-takata/minpac) and GNU stow.
+
+Then just run `./setup` which runs different code depending on the output of `hostname`. For each host, a list of stow directories is specified. These all live in `src/`. All of these folders have a folder structure that makes it easy to `stow` them, so `src/nvim/.config/nvim/init.vim`.
+
+There are other platform specific configurations which are not tracked in this repository, such as Systemd files, or `.desktop` files for Linux display managers.
--- src/fish/.config/fish/config.fish
+++ src/fish/.config/fish/config.fish
@@ -37,16 +37,11 @@ set -x PATH                 \
     ~/.emacs.d/bin          \
     $PATH
 
-set -x NIX_PATH ~/.nix-defexpr/channels $NIX_PATH
-
 abbr -a pbc 'xclip -selection clipboard'
 abbr -a g 'git'
-abbr -a todo 'rg --heading "\s*\* TODO" $NOTES_DIR | string trim'
-abbr -a maybe 'rg --heading "\s*\* MAYBE" $NOTES_DIR | string trim'
-abbr -a inprogress 'rg --heading "\s*\* IN PROGRESS" $NOTES_DIR | string trim'
-abbr -a devdiary 'set FISH_JOURNAL_DIR ~/work/devdiary; journal'
 abbr -a ovr 'exa -la --tree --git-ignore --git -I ".git"'
-abbr -a note 'nvim $NOTES_DIR/'
+abbr -a nsc 'notes search_content | rg body | xargs bat'
+abbr -a nst 'notes search_tags | rg body | xargs bat'
 alias ls exa
 alias fzf 'fzf --color=light'
 
@@ -55,71 +50,14 @@ if test -d /home/tifa/.opam/opam-init/init.fish
     source /home/tifa/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 end
 
-switch (hostname)
-    case archminimal
-        set -x FISH_NOTES_DIR /data/fish_notes
-        set -x FISH_JOURNAL_DIR /data/fish_journal
-        set -x SHELL /usr/bin/fish
-    case archdesktop
-        set -x FISH_NOTES_DIR /data/fish_notes
-        set -x FISH_JOURNAL_DIR /data/fish_journal
-        set -x SHELL /usr/bin/fish
-    case fbs-work.local
-        set -x FISH_NOTES_DIR $XDG_DATA_HOME/fish_notes
-        set -x FISH_JOURNAL_DIR $XDG_DATA_HOME/fish_journal
-        set -x SHELL (which fish)
-
-        contains /usr/local/opt/coreutils/libexec/gnubin $PATH
-        or set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-
-        contains /opt/local/bin $PATH
-        or set -x PATH /opt/local/bin $PATH
+# Source hostname specific stuff
+set -l hostname_file $XDG_CONFIG_HOME/fish/(hostname).fish
+if test -e $hostname_file
+    source $hostname_file
 end
 
-function __handle_notes
-  read -la f
-
-  for v in $f
-      set -l dir (dirname $v)
-
-      echo "-------------------------------------------------------------"
-
-      printf "Title: %s\n" (cat $dir/title)
-      printf "Tags: %s\n" (cat $dir/tags)
-      printf "Date: %s\n" (cat $dir/date)
-      echo ""
-      fold $dir/body*
-
-      echo ""
-      echo ""
-  end
-end
-
-function notes_by_title
-  rg --files-with-matches -S $argv[1] $FISH_NOTES_DIR/*/title | __handle_notes | bat
-end
-
-function notes_by_tags
-  set -l results $FISH_NOTES_DIR/*/tags
-
-  for tag in $argv
-      set results (grep -l "$tag" $results)
-  end
-
-  echo "$results" | __handle_notes | bat
-end
-
-function notes_all
-  set -l notes $FISH_NOTES_DIR/*/title
-
-  echo "$notes" | __handle_notes | bat
-end
-
-function agenda
-  $EDITOR (dirname (rg --files-with-matches -wF '#TODO#' $FISH_NOTES_DIR/*/title))/body*
-end
-
-function agenda_work
-  $EDITOR (dirname (rg --files-with-matches -wF '#TODO_WORK#' $FISH_NOTES_DIR/*/title))/body*
+# https://direnv.net/docs/hook.html
+if type -q direnv
+    eval (direnv hook fish)
 end
 
