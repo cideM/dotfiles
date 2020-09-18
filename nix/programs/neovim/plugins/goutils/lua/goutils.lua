11e9999481b153e8be8d0dac8e48e1526e990f62
Florian Beeres <yuuki@protonmail.com>
2020-09-18 09:35:46 +0200

    Improve goutils
    - Add float win for test output

--- nix/programs/neovim/plugins/goutils/lua/goutils.lua
+++ nix/programs/neovim/plugins/goutils/lua/goutils.lua
@@ -1,4 +1,34 @@
+local a = vim.api
+
 module = {}
+
+local test_options = {
+    height = 75,
+    width = 100,
+    relative = "win",
+    win = 0,
+    anchor = "SW",
+    style = "minimal",
+    focusable = true
+}
+
+local function render_floating_window(options)
+    local opts = options or test_options
+
+    -- Get editor dimensions (not window/pane!)
+    local width = a.nvim_get_option("columns")
+    local height = a.nvim_get_option("lines")
+
+    local buf = a.nvim_create_buf(false, true)
+    a.nvim_buf_set_option(buf, 'bufhidden', 'delete')
+    opts.width = width
+    opts.height = height / 2
+    opts.row = width-1
+    opts.col = 0
+    local win = a.nvim_open_win(buf, 0, opts)
+
+    return buf, win 
+end
 
 -- Temporarily switch to directory of current buffer and call "go test" This
 -- solves the problem of having to figure out the correct path to the current
@@ -10,24 +40,59 @@ function module.run_tests(testname)
 
     vim.cmd("silent cd " .. cbd)
 
+    local buf, win = render_floating_window()
+
     local cmd = testname and ("go test -run " .. testname) or "go test"
 
-    -- make it transparent what we're doing
-    print("running: ", cmd)
-    local handle = io.popen(cmd)
+    local stdio = vim.loop.new_pipe(false)
+    local stdout = vim.loop.new_pipe(false)
+    local stderr = vim.loop.new_pipe(false)
 
-    for line in handle:lines() do
-        -- > gsub also returns, as its second value, the total number 
-        -- > of matches that occurred
-        -- We don't need that
-        local l = string.gsub(line, "\t", "")
-        print(l)
-    end
+    local out = {"running: " .. cmd}
 
-    handle:close()
+    local handle
+    local pid
+    handle, pid = vim.loop.spawn("go", {
+            args = {"test"},
+            stdio = {stdio, stdout, stderr}
+        }, 
+        vim.schedule_wrap(function()
+            stdout:read_stop()
+            stderr:read_stop()
+            stdout:close()
+            stderr:close()
+            handle:close()
+
+            for _ , line in ipairs(out) do
+                local l = string.gsub(line, "\t", ": ")
+                local last_line = vim.fn.line('$')-1
+                a.nvim_buf_set_lines(buf, last_line, last_line, false, {l})
+            end
+        end))
+
+    vim.loop.read_start(stderr, function(err, data)
+        assert(not err, err)
+        if data then
+            print("read_start stdout", data)
+            for line in vim.gsplit(data, "\n") do
+                table.insert(out, line)
+            end
+        end
+    end)
+
+    vim.loop.read_start(stdout, function(err, data)
+        assert(not err, err)
+        if data then
+            print("read_start stdout", data)
+            for line in vim.gsplit(data, "\n") do
+                table.insert(out, line)
+            end
+        end
+    end)
+
 
     -- restore original cwd
     vim.cmd("silent cd " .. cwd)
 end
 
-return module
+    return module
