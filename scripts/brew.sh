1867b6adadf491535761901572bedb33c70c52df
Florian Beeres <yuuki@protonmail.com>
2020-09-27 14:40:57 +0200

    Cleanup

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -3,4 +3,4 @@
 # https://discourse.nixos.org/t/migrating-from-homebrew-to-nix-for-osx/2892
 
 brew leaves > pkgs/"$(hostname)".txt
-brew cask list > pkgs/"$(hostname)"_cask.txt
+brew list --cask > pkgs/"$(hostname)"_cask.txt
