32f45e64ac13176c3a64fe4e0150b8a01bb80993
Florian Beeres <yuuki@protonmail.com>
2020-09-27 21:55:00 +0200

    Remove some comments
    I now kind of understand what the snippet does

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     home-manager = {
       url = "github:nix-community/home-manager";
+      # https://github.com/nix-community/home-manager/blob/master/flake.nix#L4
+      # HM takes 'nixpkgs' as input
       inputs.nixpkgs.follows = "/unstable";
     };
 
@@ -11,10 +13,6 @@
       url = "/home/tifa/OperatorMono";
       flake = false;
     };
-
-    # nixpkgs = {
-    #   url = "github:NixOS/nixpkgs/nixos-20.03";
-    # };
 
     unstable = {
       url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -38,11 +36,6 @@
             inherit pkgs operatorMono;
           };
 
-          # https://github.com/nix-community/home-manager
-          # I have no idea what this snippet does, I just copied it from the
-          # flake docs. It creates a new module, which adds a new option, or
-          # in this case a submodule (?). It inherits specialArgs and... sets
-          # super to the super config? Hmm... *head scratching*
           hm-nixos-as-super = { config, ... }: {
             # Submodules have merge semantics, making it possible to amend
             # the `home-manager.users` submodule for additional functionality.
