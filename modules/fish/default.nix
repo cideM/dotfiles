2722bbeb520706538ab51b7853c0bfbc1aa527a9
Florian Beeres <yuuki@protonmail.com>
2021-01-08 13:13:08 +0100

    Revert "Simpler shell prompt"
    This reverts commit 0ac176f10bd9ba0a91ac8b18071c29c417a04277.

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -88,10 +88,25 @@ in
     };
 
     promptInit = ''
+      set __fish_git_prompt_show_informative_status 1
+
+      set __fish_git_prompt_char_dirtystate '+'
+      set __fish_git_prompt_char_invalidstate 'x'
+      set __fish_git_prompt_char_stagedstate '*'
+      set __fish_git_prompt_char_untrackedfiles 'u'
+      set __fish_git_prompt_char_stateseparator '|'
+
       function fish_prompt
           set_color $fish_color_cwd
           echo -n (basename $PWD)
+          fish_git_prompt
           set_color normal
+          if jobs -q
+            printf ' (%i)' (count jobs)
+          end
+          if test "$IN_LORRI_SHELL" != ""
+            echo -n ' [L] '
+          end
           echo -n ' $ '
       end
     '';
