ed7d2145ff00de858e19daa589f54ec7d0ecb847
Florian Beeres <yuuki@protonmail.com>
2021-02-02 10:46:59 +0100

    Fix VSCode overlay

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,6 +1,18 @@
 { pkgs, config, ... }:
 let
-  platform = if pkgs.stdenv.isDarwin then "darwin" else "linux-x64";
+  inherit (pkgs.stdenv.hostPlatform) system;
+
+  archive_fmt = if system == "x86_64-darwin" then "zip" else "tar.gz";
+
+  plat = {
+    x86_64-linux = "linux-x64";
+    x86_64-darwin = "darwin";
+    aarch64-linux = "linux-arm64";
+    armv7l-linux = "linux-armhf";
+  }.${system};
+
+  version = "latest";
+
 
 in
 {
@@ -17,9 +29,10 @@ in
         # changes. When Nix then downloads the source, it sees a different
         # hash. Long story short: I'll just have to update the hash here all
         # the time and can't use 'niv' for this.
-        src = builtins.fetchTarball {
-          url = "https://vscode-update.azurewebsites.net/latest/${platform}/insider";
-          sha256 = "0xi0zv8i4vslwyb1k5l81aff81xim915hyw2pjkyb1xqxfjs4xqb";
+        src = builtins.fetchurl {
+          name = "VSCode_${version}_${plat}.${archive_fmt}";
+          url = "https://vscode-update.azurewebsites.net/${version}/${plat}/insider";
+          sha256 = "0ys35k63c5npn30xich5787dks2pspa872ma1k7jkf9dzj75cxz8";
         };
       });
   };
