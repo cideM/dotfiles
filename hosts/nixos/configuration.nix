47a997df9d7d06706183d1251a74d409422f85e7
Florian Beeres <yuuki@protonmail.com>
2021-11-11 23:01:34 +0100

    Use latest kernel

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -21,6 +21,7 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "processor.max_cstate=1"
   ];
