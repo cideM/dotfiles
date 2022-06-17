6d32d70cf2bb35f14b7f3d73706702595e244ae2
cideM <cideM@users.noreply.github.com>
2022-06-17 08:05:48 +0000

    Updated Visual Studio Code extensions

--- modules/vscode/shared_exts.nix
+++ modules/vscode/shared_exts.nix
@@ -40,7 +40,7 @@
 { name = "find-jump"; publisher = "mksafi"; version = "1.2.4"; sha256 = "1qk2sl3dazna3zg6nq2m7313jdl67kxm5d3rq0lfmi6k1q2h9sd7"; }
 { name = "theme-monokai-pro-vscode"; publisher = "monokai"; version = "1.1.20"; sha256 = "0ddwqsvsqdjblmb0xlad17czy2837g27ymwvzissz4b9r111xyhx"; }
 { name = "python"; publisher = "ms-python"; version = "2022.9.11661004"; sha256 = "0d69q54iwvydsqvjxx21h0ccqz2kx7j3nd68xidfvyf2fbrfbd3s"; }
-{ name = "jupyter"; publisher = "ms-toolsai"; version = "2022.6.1001681045"; sha256 = "1javq8r2kzdr701l7kcspy3da63i58y4m2hb25siv4wmfxjac56h"; }
+{ name = "jupyter"; publisher = "ms-toolsai"; version = "2022.6.1001691033"; sha256 = "1fx577jq7lfkag2p80brmlxhjfp9yj4wm03bh8w6g00g609jjhs2"; }
 { name = "remote-containers"; publisher = "ms-vscode-remote"; version = "0.240.0"; sha256 = "1cxaq9qsvkky76cyl4rilwy7xiavvz6fcz5pyiz76sdq60nicrzw"; }
 { name = "remote-ssh"; publisher = "ms-vscode-remote"; version = "0.83.2022061415"; sha256 = "04851vbwxky120rpw1sxr59fc3dm9a4245b18038sgn0d6p2kxsp"; }
 { name = "remote-ssh-edit"; publisher = "ms-vscode-remote"; version = "0.80.0"; sha256 = "0zgrd2909xpr3416cji0ha3yl6gl2ry2f38bvx4lsjfmgik0ic6s"; }
@@ -64,7 +64,7 @@
 { name = "hfmt-vscode"; publisher = "sergey-kintsel"; version = "0.1.1"; sha256 = "1ryfb7kp47qw4xs36fj7ij84hjv1ambvlflgjwyxwwmwx9w8vcgd"; }
 { name = "mdx"; publisher = "silvenon"; version = "0.1.0"; sha256 = "1mzsqgv0zdlj886kh1yx1zr966yc8hqwmiqrb1532xbmgyy6adz3"; }
 { name = "rewrap"; publisher = "stkb"; version = "17.8.0"; sha256 = "1y168ar01zxdd2x73ddsckbzqq0iinax2zv3d95nhwp9asjnbpgn"; }
-{ name = "lua"; publisher = "sumneko"; version = "3.3.0"; sha256 = "16d99b4gcsm421r4kmaxr250va60am0n5jg255a64hsx65j08zfb"; }
+{ name = "lua"; publisher = "sumneko"; version = "3.3.1"; sha256 = "03xamlmj1nafx5f3ppkr6kaybin1ynq5l9gx19sir1yhx4i988w2"; }
 { name = "sass-indented"; publisher = "syler"; version = "1.8.20"; sha256 = "1q1vnmd8h5918hi08hl083kdsvrqgzb0v97ihi99ahwpkahwlxy5"; }
 { name = "language-stylus"; publisher = "sysoev"; version = "1.15.0"; sha256 = "0xryxmypwxlrq43nnmmi8x2n1476qw6ii20gcmn7kcpnrznj97f4"; }
 { name = "latex-utilities"; publisher = "tecosaur"; version = "0.3.7"; sha256 = "05clfs2bkyd9m905m5xcs04lwwdacbzy2r0k18hw47jrl60pkkgw"; }
