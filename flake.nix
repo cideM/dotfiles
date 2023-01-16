80f87ccf785379c56087943b3073cf257a381118
Florian Beeres <yuuki@protonmail.com>
2023-01-16 12:10:20 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -122,7 +122,7 @@
       (self: super: let
         shas = {
           "aarch64-darwin" = "sha256-P/jW95Udtw9ZjlS/ipaOTCup56fAZAXktHsUbJV+GxY=";
-          "x86_64-linux" = "sha256-P/jW95Udtw9ZjlS/ipaOTCup56fAZAXktHsUbJV+GxY=";
+          "x86_64-linux" = "sha256-6DHzMeNREoJT4fsRKDmB/VcfEUkAWAx0CnL5YDVNIt4=";
         };
       in {
         volta = super.rustPlatform.buildRustPackage rec {
