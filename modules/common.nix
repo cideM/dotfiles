ffec2465606ed6e357100d69e0e32e999286c74a
Florian Beeres <yuuki@protonmail.com>
2023-01-19 12:27:55 +0100

    wezterm: tweak config

--- modules/common.nix
+++ modules/common.nix
@@ -59,14 +59,24 @@ with pkgs;
     return {
       color_scheme = "Edge Light (base16)",
       default_prog = { '${pkgs.fish}/bin/fish', '-l' },
-      font_size = 16.0,
+      font_size = 15.0,
       font = wezterm.font('Operator Mono SSm', { weight = 'Book' }),
-      line_height = 1.2,
+      line_height = 1.1,
       keys = {
         {
           key = 'w',
           mods = 'CMD',
           action = wezterm.action.CloseCurrentPane { confirm = true },
+        },
+        {
+          key = '-',
+          mods = 'CMD',
+          action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+        },
+        {
+          key = '|',
+          mods = 'CMD|SHIFT',
+          action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
         },
       },
     }
