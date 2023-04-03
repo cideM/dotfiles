cdcf59b27c1ef58001c20dc514d56bc06141f477
Florian Beeres <yuuki@protonmail.com>
2023-04-03 17:46:30 +0200

    Use one light in Alacritty again

--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -6,7 +6,7 @@
 }: {
   programs.alacritty = {
     settings = {
-      colors = (builtins.fromJSON (builtins.readFile "${pkgs.rose-pine-alacritty}/rose-pine-dawn.json")).colors;
+      colors = (import ./colors.nix).oneLight;
       font = (import ./fonts.nix).mono;
       key_bindings = [
         {
