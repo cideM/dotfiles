c801b8db322d212f54b04ae388525a73edd0455e
Florian Beeres <yuuki@protonmail.com>
2023-07-02 12:14:48 +0200

    vim: disable loading some builtins

--- modules/vim.nix
+++ modules/vim.nix
@@ -40,6 +40,25 @@ args @ {
             echo synIDattr(l:s, 'name') . ' -> ' . synIDattr(synIDtrans(l:s), 'name')
         endfun
 
+        let g:loaded_gzip = 1
+        let g:loaded_zip = 1
+        let g:loaded_zipPlugin = 1
+        let g:loaded_tar = 1
+        let g:loaded_tarPlugin = 1
+
+        let g:loaded_getscript = 1
+        let g:loaded_getscriptPlugin = 1
+        let g:loaded_vimball = 1
+        let g:loaded_vimballPlugin = 1
+        let g:loaded_2html_plugin = 1
+
+        let g:loaded_logiPat = 1
+        let g:loaded_rrhelper = 1
+
+        let g:loaded_netrw = 1
+        let g:loaded_netrwPlugin = 1
+        let g:loaded_netrwSettings = 1
+
         set hlsearch
         set visualbell
         set textwidth=80
