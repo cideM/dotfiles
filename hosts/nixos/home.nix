edb6b90102f02f03e1ca71b6cb6d0720760a77a0
Florian Beeres <yuuki@protonmail.com>
2023-07-10 11:51:51 +0200

    remove cruft from NixOS config

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -55,8 +55,6 @@
     bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "${pkgs.xsel}/bin/xsel -i --clipboard"
   '';
 
-  xdg.configFile."fish/themes/rose_pine_dawn.theme".source = "${pkgs.rose-pine-fish}/themes/Rosé Pine Dawn.theme";
-
   xdg.configFile.".gemrc".text = ''
     :ipv4_fallback_enabled: true
   '';
