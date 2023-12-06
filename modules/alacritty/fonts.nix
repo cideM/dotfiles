94d73ad33dbb3b7907cffb0bc9716599a4d341bd
Florian Beeres <yuuki@protonmail.com>
2023-12-06 13:13:26 +0100

    Less vertical spacing

--- modules/alacritty/fonts.nix
+++ modules/alacritty/fonts.nix
@@ -37,7 +37,7 @@
     };
     glyph_offset = {
       x = 0;
-      y = 2;
+      y = 1;
     };
     italic = {
       family = "Operator Mono SSm";
@@ -49,7 +49,7 @@
     };
     offset = {
       x = 0;
-      y = 4;
+      y = 2;
     };
   };
 
