08efd9f01bb71095f8345f21b051450e829c0318
Florian Beeres <yuuki@protonmail.com>
2025-02-07 14:36:43 +0100

    remove tcl + tclreadline again
    tclreadline + tcl9 doesn't work. I'll now go with a project based tcl9 +
    rlwrap

--- modules/common.nix
+++ modules/common.nix
@@ -1,206 +1,193 @@
-{pkgs, ...}: let
-  tclreadline = pkgs.fetchFromGitHub {
-    owner = "flightaware";
-    repo = "tclreadline";
-    rev = pkgs.tclreadline.version or "v2.4.0";
-    sha256 = "sha256-rB2bR0yu/ZFf/WOgo1LeLmciaQA42/LulnqSczmzea8=";
+{pkgs, ...}:
+with pkgs; {
+  home.packages = [
+    bashInteractive
+    bat
+    btop
+    coreutils-full
+    curl
+    du-dust
+    dua
+    entr
+    fastmod
+    fd
+    findutils
+    gawk
+    gh
+    git-lfs
+    github-markdown-toc
+    glow
+    gnugrep
+    gnupg
+    gnused
+    gzip
+    haskellPackages.hledger_1_41
+    helix
+    htop
+    hyperfine
+    jq
+    micro
+    monaspace
+    nano
+    nixpkgs-review
+    oils-for-unix
+    rclone
+    ripgrep
+    rlwrap
+    shellcheck
+    tasksh
+    time
+    timewarrior
+    tokei
+    tree
+    uni
+    universal-ctags
+    unzip
+    vis
+    wget
+  ];
+
+  programs.taskwarrior = {
+    package = pkgs.taskwarrior3;
+    enable = true;
+    colorTheme = "no-color";
+    config = {
+      news.version = "3.3.0";
+      context = {
+        work = {
+          read = "+work";
+          write = "+work";
+        };
+        home = {
+          read = "-work +home";
+          write = "+home";
+        };
+      };
+    };
   };
-in
-  with pkgs; {
-    home.packages = [
-      bashInteractive
-      bat
-      btop
-      coreutils-full
-      curl
-      du-dust
-      dua
-      entr
-      fastmod
-      fd
-      findutils
-      gawk
-      gh
-      git-lfs
-      github-markdown-toc
-      glow
-      gnugrep
-      gnupg
-      gnused
-      gzip
-      haskellPackages.hledger_1_41
-      helix
-      htop
-      hyperfine
-      jq
-      micro
-      monaspace
-      nano
-      nixpkgs-review
-      oils-for-unix
-      rclone
-      ripgrep
-      rlwrap
-      shellcheck
-      tasksh
-      tclreadline
-      tcl-9_0
-      time
-      timewarrior
-      tokei
-      tree
-      uni
-      universal-ctags
-      unzip
-      vis
-      wget
+
+  nix = {
+    gc = {
+      automatic = true;
+      frequency = "daily";
+    };
+  };
+
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    VISUAL = "nvim";
+    EDITOR = "nvim";
+    SHELL = "${pkgs.fish}/bin/fish";
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+    changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
+    changeDirWidgetOptions = [
+      "--preview '${pkgs.eza}/bin/eza --oneline --git --long {}'"
+      "--color=light"
     ];
+    historyWidgetOptions = [
+      "--sort"
+    ];
+    fileWidgetCommand = "fd --type f --hidden --follow --exclude .git";
+    fileWidgetOptions = [
+      "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range :300 {}'"
+      "--color=light"
+    ];
+    defaultCommand = "fd --type f --hidden --follow --exclude .git";
+    defaultOptions = [
+      "--color=light"
+    ];
+  };
 
-    programs.taskwarrior = {
-      package = pkgs.taskwarrior3;
+  programs.dircolors = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
       enable = true;
-      colorTheme = "no-color";
-      config = {
-        news.version = "3.3.0";
-        context = {
-          work = {
-            read = "+work";
-            write = "+work";
-          };
-          home = {
-            read = "-work +home";
-            write = "+home";
-          };
+    };
+  };
+
+  programs.nnn = {
+    enable = true;
+    plugins = {
+      mappings = {
+        p = "preview-tui";
+      };
+      src =
+        (pkgs.fetchFromGitHub {
+          owner = "jarun";
+          repo = "nnn";
+          rev = "v5.0";
+          sha256 = "sha256-HShHSjqD0zeE1/St1Y2dUeHfac6HQnPFfjmFvSuEXUA=";
+        })
+        + "/plugins";
+    };
+  };
+
+  programs.man.enable = true;
+
+  xdg.dataFile."task/hooks/on-modify.timewarrior" = {
+    executable = true;
+    source = "${pkgs.timewarrior}/share/doc/timew/ext/on-modify.timewarrior";
+  };
+
+  xdg.configFile.".gemrc".text = ''
+    :ipv4_fallback_enabled: true
+  '';
+
+  home.file = {
+    ".gitignore" = {
+      text = ''
+        .direnv
+      '';
+    };
+
+    "/Library/Preferences/glow/glow.yml" = {
+      text = ''
+        # style name or JSON path (default "auto")
+        style: "light"
+        # show local files only; no network (TUI-mode only)
+        local: true
+        # mouse support (TUI-mode only)
+        mouse: false
+        # use pager to display markdown
+        pager: false
+        # word-wrap at width
+        width: 80
+      '';
+    };
+  };
+
+  # https://github.com/rycee/home-manager/issues/432
+  home.extraOutputsToInstall = ["info" "man" "share" "icons" "doc"];
+
+  xdg.configFile."git/attributes".text = ''
+    *.lockb binary diff=lockb
+  '';
+
+  xdg.configFile."nix/registry.json".text = builtins.toJSON {
+    version = 2;
+    flakes = {
+      fbrs = {
+        from = {
+          id = "fbrs";
+          type = "indirect";
+        };
+        to = {
+          type = "github";
+          owner = "cidem";
+          repo = "nix-templates";
         };
       };
     };
-
-    nix = {
-      gc = {
-        automatic = true;
-        frequency = "daily";
-      };
-    };
-
-    home.sessionVariables = {
-      LANG = "en_US.UTF-8";
-      LC_ALL = "en_US.UTF-8";
-      LC_CTYPE = "en_US.UTF-8";
-      VISUAL = "nvim";
-      EDITOR = "nvim";
-      SHELL = "${pkgs.fish}/bin/fish";
-    };
-
-    programs.fzf = {
-      enable = true;
-      enableFishIntegration = true;
-      changeDirWidgetCommand = "fd --type d --hidden --follow --exclude .git";
-      changeDirWidgetOptions = [
-        "--preview '${pkgs.eza}/bin/eza --oneline --git --long {}'"
-        "--color=light"
-      ];
-      historyWidgetOptions = [
-        "--sort"
-      ];
-      fileWidgetCommand = "fd --type f --hidden --follow --exclude .git";
-      fileWidgetOptions = [
-        "--preview '${pkgs.bat}/bin/bat --color=always --style=numbers --line-range :300 {}'"
-        "--color=light"
-      ];
-      defaultCommand = "fd --type f --hidden --follow --exclude .git";
-      defaultOptions = [
-        "--color=light"
-      ];
-    };
-
-    programs.dircolors = {
-      enable = true;
-      enableFishIntegration = true;
-    };
-
-    programs.direnv = {
-      enable = true;
-      nix-direnv = {
-        enable = true;
-      };
-    };
-
-    programs.nnn = {
-      enable = true;
-      plugins = {
-        mappings = {
-          p = "preview-tui";
-        };
-        src =
-          (pkgs.fetchFromGitHub {
-            owner = "jarun";
-            repo = "nnn";
-            rev = "v5.0";
-            sha256 = "sha256-HShHSjqD0zeE1/St1Y2dUeHfac6HQnPFfjmFvSuEXUA=";
-          })
-          + "/plugins";
-      };
-    };
-
-    programs.man.enable = true;
-
-    xdg.dataFile."task/hooks/on-modify.timewarrior" = {
-      executable = true;
-      source = "${pkgs.timewarrior}/share/doc/timew/ext/on-modify.timewarrior";
-    };
-
-    xdg.configFile.".gemrc".text = ''
-      :ipv4_fallback_enabled: true
-    '';
-
-    home.file = {
-      ".tclshrc" = {
-        text = builtins.readFile "${tclreadline}/sample.tclshrc";
-      };
-
-      ".gitignore" = {
-        text = ''
-          .direnv
-        '';
-      };
-
-      "/Library/Preferences/glow/glow.yml" = {
-        text = ''
-          # style name or JSON path (default "auto")
-          style: "light"
-          # show local files only; no network (TUI-mode only)
-          local: true
-          # mouse support (TUI-mode only)
-          mouse: false
-          # use pager to display markdown
-          pager: false
-          # word-wrap at width
-          width: 80
-        '';
-      };
-    };
-
-    # https://github.com/rycee/home-manager/issues/432
-    home.extraOutputsToInstall = ["info" "man" "share" "icons" "doc"];
-
-    xdg.configFile."git/attributes".text = ''
-      *.lockb binary diff=lockb
-    '';
-
-    xdg.configFile."nix/registry.json".text = builtins.toJSON {
-      version = 2;
-      flakes = {
-        fbrs = {
-          from = {
-            id = "fbrs";
-            type = "indirect";
-          };
-          to = {
-            type = "github";
-            owner = "cidem";
-            repo = "nix-templates";
-          };
-        };
-      };
-    };
-  }
+  };
+}
