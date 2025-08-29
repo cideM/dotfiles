503582568a21fba87f69824adf8cdd59b78464c1
Florian Beeres <yuuki@protonmail.com>
2025-08-29 11:07:18 +0200

    shift enter for claude

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -22,6 +22,7 @@ args@{
     adjust-cell-width = -6%
     theme = light:yui_light,dark:yui_dark
     window-padding-x = 20
+    keybind = shift+enter=text:\x1b\r
     window-padding-balance = true
     font-codepoint-map = U+00A4-U+00F7,U+03B7-U+03C4,U+2045-U+2099,U+2102,U+2198-U+2369,U+25A1-U+25FF,U+266D-U+2682,U+27DC,U+2919-U+2938,U+29B7-U+2A2C,U+2B1A=Uiua386
   '';
