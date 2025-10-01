ff5078e29e7e37c594c16cd2256908d5aa5a108d
Florian Beeres <yuuki@protonmail.com>
2025-10-01 13:36:39 +0200

    add claude to devshell

--- flake.nix
+++ flake.nix
@@ -52,194 +52,203 @@
     };
   };
 
-  outputs = {
-    self,
-    janet-vim,
-    github-nvim-theme-src,
-    home-manager,
-    flake-utils,
-    ghostty,
-    neovim-nightly-overlay,
-    operatorMono,
-    github-markdown-toc-go-src,
-    nixpkgs,
-    nix-fish-src,
-    lucid-fish-prompt-src,
-    zig-overlay,
-    yui,
-    vim-js,
-  } @ inputs: let
-    overlays = [
-      (final: prev: rec {zigpkgs = zig-overlay.packages.${prev.system};})
+  outputs =
+    {
+      self,
+      janet-vim,
+      github-nvim-theme-src,
+      home-manager,
+      flake-utils,
+      ghostty,
+      neovim-nightly-overlay,
+      operatorMono,
+      github-markdown-toc-go-src,
+      nixpkgs,
+      nix-fish-src,
+      lucid-fish-prompt-src,
+      zig-overlay,
+      yui,
+      vim-js,
+    }@inputs:
+    let
+      overlays = [
+        (final: prev: rec { zigpkgs = zig-overlay.packages.${prev.system}; })
 
-      (self: super: {
-        github-markdown-toc = super.buildGoModule {
-          name = "github-markdown-toc-go";
-          version = "latest";
-          src = github-markdown-toc-go-src;
-          vendorHash = "sha256-K5yb7bnW6eS5UESK9wgNEUwGjB63eJk6+B0jFFiFero=";
-        };
-      })
+        (self: super: {
+          github-markdown-toc = super.buildGoModule {
+            name = "github-markdown-toc-go";
+            version = "latest";
+            src = github-markdown-toc-go-src;
+            vendorHash = "sha256-K5yb7bnW6eS5UESK9wgNEUwGjB63eJk6+B0jFFiFero=";
+          };
+        })
 
-      (self: super: {
-        yui-ghostty-theme = yui.packages.${super.system}.ghostty;
-      })
+        (self: super: {
+          yui-ghostty-theme = yui.packages.${super.system}.ghostty;
+        })
 
-      (self: super: {
-        yui-alacritty-theme = yui.packages.${super.system}.alacritty;
-      })
+        (self: super: {
+          yui-alacritty-theme = yui.packages.${super.system}.alacritty;
+        })
 
-      (self: super: {
-        vimPlugins =
-          super.vimPlugins
-          // {
+        (self: super: {
+          vimPlugins = super.vimPlugins // {
             yui = yui.packages.${super.system}.neovim;
           };
-      })
+        })
 
-      (self: super: {
-        vimPlugins =
-          super.vimPlugins
-          // {
+        (self: super: {
+          vimPlugins = super.vimPlugins // {
             github-nvim-theme = super.pkgs.vimUtils.buildVimPlugin rec {
               version = "latest";
               pname = "github-nvim-theme";
               src = github-nvim-theme-src;
             };
           };
-      })
+        })
 
-      (self: super: {
-        operatorMonoFont = super.pkgs.stdenv.mkDerivation {
-          name = "operator-mono-font";
-          src = operatorMono;
-          buildPhases = ["installPhase"];
-          installPhase = ''
-            mkdir -p $out/share/fonts/operator-mono
-            cp -R "$src" "$out/share/fonts/operator-mono"
-          '';
-        };
-      })
+        (self: super: {
+          operatorMonoFont = super.pkgs.stdenv.mkDerivation {
+            name = "operator-mono-font";
+            src = operatorMono;
+            buildPhases = [ "installPhase" ];
+            installPhase = ''
+              mkdir -p $out/share/fonts/operator-mono
+              cp -R "$src" "$out/share/fonts/operator-mono"
+            '';
+          };
+        })
 
-      (self: super: {
-        vimPlugins =
-          super.vimPlugins
-          // {
+        (self: super: {
+          vimPlugins = super.vimPlugins // {
             janet-vim = super.pkgs.vimUtils.buildVimPlugin rec {
               version = "latest";
               pname = "janet-vim";
               src = janet-vim;
             };
           };
-      })
+        })
 
-      (self: super: {
-        vimPlugins =
-          super.vimPlugins
-          // {
+        (self: super: {
+          vimPlugins = super.vimPlugins // {
             vim-js = super.pkgs.vimUtils.buildVimPlugin rec {
               version = "latest";
               pname = "vim-js";
               src = vim-js;
             };
           };
-      })
+        })
 
-      (final: prev: {
-        fishPlugins = prev.fishPlugins.overrideScope (finalx: prevx: {
-          yui = yui.packages.${final.system}.fish_light;
-        });
-      })
+        (final: prev: {
+          fishPlugins = prev.fishPlugins.overrideScope (
+            finalx: prevx: {
+              yui = yui.packages.${final.system}.fish_light;
+            }
+          );
+        })
 
-      (self: super: {
-        oil = super.oil.overrideAttrs (old: {
-          env.NIX_CFLAGS_COMPILE = super.lib.optionalString super.stdenv.cc.isClang "-Wno-error=incompatible-function-pointer-types";
-        });
-      })
+        (self: super: {
+          oil = super.oil.overrideAttrs (old: {
+            env.NIX_CFLAGS_COMPILE = super.lib.optionalString super.stdenv.cc.isClang "-Wno-error=incompatible-function-pointer-types";
+          });
+        })
 
-      (self: super: {
-        lucid-fish-prompt = super.pkgs.stdenv.mkDerivation {
-          pname = "lucid-fish-prompt";
-          version = "latest";
-          src = lucid-fish-prompt-src;
-        };
-      })
-
-      (self: super: {
-        nix-fish = super.pkgs.stdenv.mkDerivation rec {
-          pname = "nix-fish";
-          version = "latest";
-          src = nix-fish-src;
-        };
-      })
-    ];
-
-    specialArgs = {
-      inherit home-manager inputs;
-    };
-
-    homeConfigurations = {
-      work-m1 = home-manager.lib.homeManagerConfiguration rec {
-        extraSpecialArgs = specialArgs;
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-        };
-        modules = [
-          {
-            home = {
-              homeDirectory = "/Users/fbs";
-              username = "fbs";
-            };
-          }
-          {
-            nixpkgs.overlays = overlays;
-            nixpkgs.config = {
-              allowUnfree = true;
-            };
-          }
-          ./hosts/fbs-work.local/home.nix
-        ];
-      };
-    };
-
-    desktop = let
-      system = "x86_64-linux";
-
-      modules = [
-        ./hosts/nixos/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          nixpkgs.overlays =
-            overlays
-            ++ [
-              (self: super: {
-                ghostty = ghostty.packages.${super.system}.default;
-              })
-            ];
-          nixpkgs.config = {
-            allowUnfree = true;
+        (self: super: {
+          lucid-fish-prompt = super.pkgs.stdenv.mkDerivation {
+            pname = "lucid-fish-prompt";
+            version = "latest";
+            src = lucid-fish-prompt-src;
           };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = false;
-          home-manager.verbose = true;
-          home-manager.backupFileExtension = "hm-backup";
-          home-manager.users.fbrs = import ./hosts/nixos/home.nix;
-          home-manager.extraSpecialArgs = specialArgs;
-        }
+        })
+
+        (self: super: {
+          nix-fish = super.pkgs.stdenv.mkDerivation rec {
+            pname = "nix-fish";
+            version = "latest";
+            src = nix-fish-src;
+          };
+        })
       ];
+
+      specialArgs = {
+        inherit home-manager inputs;
+      };
+
+      homeConfigurations = {
+        work-m1 = home-manager.lib.homeManagerConfiguration rec {
+          extraSpecialArgs = specialArgs;
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+          };
+          modules = [
+            {
+              home = {
+                homeDirectory = "/Users/fbs";
+                username = "fbs";
+              };
+            }
+            {
+              nixpkgs.overlays = overlays;
+              nixpkgs.config = {
+                allowUnfree = true;
+              };
+            }
+            ./hosts/fbs-work.local/home.nix
+          ];
+        };
+      };
+
+      desktop =
+        let
+          system = "x86_64-linux";
+
+          modules = [
+            ./hosts/nixos/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.overlays = overlays ++ [
+                (self: super: {
+                  ghostty = ghostty.packages.${super.system}.default;
+                })
+              ];
+              nixpkgs.config = {
+                allowUnfree = true;
+              };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = false;
+              home-manager.verbose = true;
+              home-manager.backupFileExtension = "hm-backup";
+              home-manager.users.fbrs = import ./hosts/nixos/home.nix;
+              home-manager.extraSpecialArgs = specialArgs;
+            }
+          ];
+        in
+        nixpkgs.lib.nixosSystem { inherit system modules specialArgs; };
     in
-      nixpkgs.lib.nixosSystem {inherit system modules specialArgs;};
-  in
     {
       nixosConfigurations.nixos = desktop;
       inherit homeConfigurations;
     }
     // flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
+      in
+      rec {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [janet jpm nixfmt nodePackages.prettier lua-language-server lua stylua];
+          buildInputs = with pkgs; [
+            janet
+            jpm
+            nixfmt
+            nodePackages.prettier
+            lua-language-server
+            lua
+            stylua
+            claude-code
+          ];
         };
       }
     );
