f1a7495ebdce3dc644f50d88a96d485128958e14
Florian Beeres <yuuki@protonmail.com>
2025-11-04 22:26:22 +0100

    Switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -23,7 +23,10 @@
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     nix-fish-src.url = "github:kidonng/nix.fish";
     nix-fish-src.flake = false;
@@ -49,151 +52,155 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
-      janet-vim,
-      home-manager,
-      flake-utils,
-      neovim-nightly-overlay,
-      operatorMono,
-      nvim-alabaster-scheme-src,
-      github-markdown-toc-go-src,
+      flake-parts,
       nixpkgs,
-      nix-fish-src,
-      zig-overlay,
-      yui,
-      vim-js,
-      sops-nix,
-    }@inputs:
-    let
-      overlays = [
-        (final: prev: rec { zigpkgs = zig-overlay.packages.${prev.system}; })
-
-        (self: super: {
-          github-markdown-toc = super.buildGoModule {
-            name = "github-markdown-toc-go";
-            version = "latest";
-            src = github-markdown-toc-go-src;
-            vendorHash = "sha256-K5yb7bnW6eS5UESK9wgNEUwGjB63eJk6+B0jFFiFero=";
-          };
-        })
-
-        (self: super: {
-          yui-ghostty-theme = yui.packages.${super.system}.ghostty;
-        })
-
-        (self: super: {
-          vimPlugins = super.vimPlugins // {
-            yui = yui.packages.${super.system}.neovim;
-          };
-        })
-
-        (self: super: {
-          operatorMonoFont = super.pkgs.stdenv.mkDerivation {
-            name = "operator-mono-font";
-            src = operatorMono;
-            buildPhases = [ "installPhase" ];
-            installPhase = ''
-              mkdir -p $out/share/fonts/operator-mono
-              cp -R "$src" "$out/share/fonts/operator-mono"
-            '';
-          };
-        })
-
-        (self: super: {
-          vimPlugins = super.vimPlugins // {
-            janet-vim = super.pkgs.vimUtils.buildVimPlugin rec {
-              version = "latest";
-              pname = "janet-vim";
-              src = janet-vim;
-            };
-          };
-        })
-
-        (self: super: {
-          vimPlugins = super.vimPlugins // {
-            nvim-alabaster-theme = super.pkgs.vimUtils.buildVimPlugin rec {
-              version = "latest";
-              pname = "nvim-alabaster-theme";
-              src = nvim-alabaster-scheme-src;
-            };
-          };
-        })
-
-        (self: super: {
-          vimPlugins = super.vimPlugins // {
-            vim-js = super.pkgs.vimUtils.buildVimPlugin rec {
-              version = "latest";
-              pname = "vim-js";
-              src = vim-js;
-            };
-          };
-        })
-
-        (final: prev: {
-          fishPlugins = prev.fishPlugins.overrideScope (
-            finalx: prevx: { yui = yui.packages.${final.system}.fish_light; }
-          );
-        })
-
-        (self: super: {
-          nix-fish = super.pkgs.stdenv.mkDerivation rec {
-            pname = "nix-fish";
-            version = "latest";
-            src = nix-fish-src;
-          };
-        })
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
-      specialArgs = { inherit home-manager inputs; };
+      flake =
+        let
+          inherit (inputs)
+            janet-vim
+            home-manager
+            neovim-nightly-overlay
+            operatorMono
+            nvim-alabaster-scheme-src
+            github-markdown-toc-go-src
+            nix-fish-src
+            zig-overlay
+            yui
+            vim-js
+            sops-nix
+            ;
 
-      homeConfigurations = {
-        work-m1 = home-manager.lib.homeManagerConfiguration rec {
-          extraSpecialArgs = specialArgs;
-          pkgs = import nixpkgs { system = "aarch64-darwin"; };
-          modules = [
-            {
-              home = {
-                homeDirectory = "/Users/fbs";
-                username = "fbs";
-              };
-            }
-            {
-              nixpkgs.overlays = overlays;
-              nixpkgs.config = {
-                allowUnfree = true;
-              };
-            }
-            ./hosts/fbs-work.local/home.nix
-          ];
-        };
-      };
+          overlays = [
+            (final: prev: rec { zigpkgs = zig-overlay.packages.${prev.system}; })
 
-    in
-    {
-      inherit homeConfigurations;
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          system = system;
-          config.allowUnfree = true;
-        };
-      in
-      rec {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            janet
-            jpm
-            nixfmt
-            nodePackages.prettier
-            lua-language-server
-            lua
-            stylua
-            claude-code
+            (self: super: {
+              github-markdown-toc = super.buildGoModule {
+                name = "github-markdown-toc-go";
+                version = "latest";
+                src = github-markdown-toc-go-src;
+                vendorHash = "sha256-K5yb7bnW6eS5UESK9wgNEUwGjB63eJk6+B0jFFiFero=";
+              };
+            })
+
+            (self: super: {
+              yui-ghostty-theme = yui.packages.${super.system}.ghostty;
+            })
+
+            (self: super: {
+              vimPlugins = super.vimPlugins // {
+                yui = yui.packages.${super.system}.neovim;
+              };
+            })
+
+            (self: super: {
+              operatorMonoFont = super.pkgs.stdenv.mkDerivation {
+                name = "operator-mono-font";
+                src = operatorMono;
+                buildPhases = [ "installPhase" ];
+                installPhase = ''
+                  mkdir -p $out/share/fonts/operator-mono
+                  cp -R "$src" "$out/share/fonts/operator-mono"
+                '';
+              };
+            })
+
+            (self: super: {
+              vimPlugins = super.vimPlugins // {
+                janet-vim = super.pkgs.vimUtils.buildVimPlugin rec {
+                  version = "latest";
+                  pname = "janet-vim";
+                  src = janet-vim;
+                };
+              };
+            })
+
+            (self: super: {
+              vimPlugins = super.vimPlugins // {
+                nvim-alabaster-theme = super.pkgs.vimUtils.buildVimPlugin rec {
+                  version = "latest";
+                  pname = "nvim-alabaster-theme";
+                  src = nvim-alabaster-scheme-src;
+                };
+              };
+            })
+
+            (self: super: {
+              vimPlugins = super.vimPlugins // {
+                vim-js = super.pkgs.vimUtils.buildVimPlugin rec {
+                  version = "latest";
+                  pname = "vim-js";
+                  src = vim-js;
+                };
+              };
+            })
+
+            (final: prev: {
+              fishPlugins = prev.fishPlugins.overrideScope (
+                finalx: prevx: { yui = yui.packages.${final.system}.fish_light; }
+              );
+            })
+
+            (self: super: {
+              nix-fish = super.pkgs.stdenv.mkDerivation rec {
+                pname = "nix-fish";
+                version = "latest";
+                src = nix-fish-src;
+              };
+            })
           ];
+
+          specialArgs = { inherit home-manager inputs; };
+        in
+        {
+          homeConfigurations = {
+            work-m1 = home-manager.lib.homeManagerConfiguration rec {
+              extraSpecialArgs = specialArgs;
+              pkgs = import nixpkgs { system = "aarch64-darwin"; };
+              modules = [
+                {
+                  home = {
+                    homeDirectory = "/Users/fbs";
+                    username = "fbs";
+                  };
+                }
+                {
+                  nixpkgs.overlays = overlays;
+                  nixpkgs.config = {
+                    allowUnfree = true;
+                  };
+                }
+                ./hosts/fbs-work.local/home.nix
+              ];
+            };
+          };
         };
-      }
-    );
+
+      perSystem =
+        { system, pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              janet
+              jpm
+              nixfmt
+              nodePackages.prettier
+              lua-language-server
+              lua
+              stylua
+            ];
+          };
+        };
+    };
 }
